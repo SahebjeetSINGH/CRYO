90a05490cf5a79199926e0325fa6313d6e30bfd6
Unnat Das <84987518+Sayo1305@users.noreply.github.com>
2022-11-06 13:48:27 +0530

    Update MainCard.jsx

--- web3-project/src/components/MainCard.jsx
+++ web3-project/src/components/MainCard.jsx
@@ -4,25 +4,29 @@ import useMetaMask from "../hooks/useMetaMask";
 import data from "../assets/data/data";
 import "../assets/css/SearchCard.css";
 import { useNavigate } from "react-router-dom";
+import CheckingIPFS from "./IPFS/CheckingIPFS";
+import nftdata from '../assets/data/NFTdata'; 
+import Modal from "./Modals";
 const MainCard = () => {
   const [Data, SetData] = useState([]);
   const navigate = useNavigate();
   const idx = localStorage.getItem("id");
   const [Show, SetShow] = useState(false);
+  const [Index  , SetIndex] = useState(0);
   // console.log(idx);
   const { connect, disconnect, balance, isActive, account } = useMetaMask();
-
   if (window.ethereum) {
-    console.log(account);
-    console.log("WALLET FOUND");
   } else {
     alert("install metamask extension!!");
   }
+  const [NFT , SetNFT] = useState([]);
   const filterdata = (idx) => {
     const tempdata = data.filter((val) => val.id === idx);
     SetData(tempdata);
-    // console.log(data);
-    // console.log(tempdata);
+    const tempnft  = nftdata.filter((val) => val.type === tempdata[0].tag);
+    SetNFT(tempnft);
+    const index = Math.floor((Math.random() * (tempnft.length) + 1));
+       SetIndex(nftdata[index].name);
   };
   useEffect(() => {
     filterdata(idx);
@@ -40,6 +44,7 @@ const MainCard = () => {
               <div>Start Time: {mp.starttime}</div>
               <div>End Time: {mp.endtime}</div>
               <div>Capacity: {mp.totalpeople}</div>
+              <div>Price: {mp.price}</div>
               <button
                 className="buyButton"
                 onClick={() => {
@@ -72,6 +77,13 @@ const MainCard = () => {
           </div>
         ))}
         <div className="Walletcontainer">Wallet : {account}</div>
+        {/* <CheckingIPFS/> */}
+        <div>
+          {
+            account && <Modal imgs ={Index}/>
+            // <img src={Index} alt="djjd"  />
+          }
+        </div>
       </div>
     </>
   );
