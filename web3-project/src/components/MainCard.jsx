4ca2c13058317b1040ed87a5907d65df2bdb3cc3
Unnat Das <udas4153@gmail.com>
2022-11-06 09:49:17 +0530

    added 70%

--- web3-project/src/components/MainCard.jsx
+++ web3-project/src/components/MainCard.jsx
@@ -1,38 +1,38 @@
 import React, { useEffect, useState } from "react";
 import { Navbar } from "react-bootstrap";
-import useMetaMask from '../hooks/useMetaMask';
+import useMetaMask from "../hooks/useMetaMask";
 import data from "../assets/data/data";
-import '../assets/css/SearchCard.css';
-import {useNavigate} from 'react-router-dom';
+import "../assets/css/SearchCard.css";
+import { useNavigate } from "react-router-dom";
 const MainCard = () => {
   const [Data, SetData] = useState([]);
   const navigate = useNavigate();
   const idx = localStorage.getItem("id");
+  const [Show, SetShow] = useState(false);
   // console.log(idx);
-  const { connect, disconnect, balance, isActive, account } = useMetaMask()
+  const { connect, disconnect, balance, isActive, account } = useMetaMask();
 
   if (window.ethereum) {
+    console.log(account);
     console.log("WALLET FOUND");
-  }
-  else{
+  } else {
     alert("install metamask extension!!");
   }
   const filterdata = (idx) => {
     const tempdata = data.filter((val) => val.id === idx);
     SetData(tempdata);
-    console.log(data);
-    console.log(tempdata);
+    // console.log(data);
+    // console.log(tempdata);
   };
   useEffect(() => {
     filterdata(idx);
-    console.log(Data);
   }, []);
   return (
     <>
       <div className="collections2">
         {Data.map((mp, idx) => (
           <div key={idx} className="CardDetail">
-              <img src={mp.img} className="CardContImg2" alt="cricket" />
+            <img src={mp.img} className="CardContImg2" alt="cricket" />
             <div className="cardDesc">
               <div>{mp.name}</div>
               <div>Location: {mp.location}</div>
@@ -40,15 +40,38 @@ const MainCard = () => {
               <div>Start Time: {mp.starttime}</div>
               <div>End Time: {mp.endtime}</div>
               <div>Capacity: {mp.totalpeople}</div>
-              <button className="buyButton">Buy Now</button>
-              <button className="backButton" onClick={()=>{navigate('/Collections')}}>Back</button>
-              <div className="connectionBtn">
-                <button className="connBtn" onClick={connect}> Connect Account</button>
-                <button className="connBtn1" onClick={disconnect}> Disconnect Account</button>
-              </div>
+              <button
+                className="buyButton"
+                onClick={() => {
+                  SetShow(!Show);
+                }}
+              >
+                Buy Now
+              </button>
+              <button
+                className="backButton"
+                onClick={() => {
+                  navigate("/Collections");
+                }}
+              >
+                Back
+              </button>
+              {Show && (
+                <div className="connectionBtn">
+                  <button className="connBtn" onClick={connect}>
+                    {" "}
+                    Connect Account
+                  </button>
+                  <button className="connBtn1" onClick={disconnect}>
+                    {" "}
+                    Disconnect Account
+                  </button>
+                </div>
+              )}
             </div>
           </div>
         ))}
+        <div className="Walletcontainer">Wallet : {account}</div>
       </div>
     </>
   );
