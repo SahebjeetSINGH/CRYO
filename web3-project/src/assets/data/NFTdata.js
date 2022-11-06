4fc1f79e8450307466aaa8c3d045197c2ad9e73f
Unnat Das <84987518+Sayo1305@users.noreply.github.com>
2022-11-06 13:46:06 +0530

    Add files via upload

---
+++ web3-project/src/assets/data/NFTdata.js
@@ -0,0 +1,59 @@
+const comicon1 = require('../images/marvel.png');
+const comicon2 = require('../images/marvel2.png');
+const comicon3 = require('../images/venom.png');
+const cricket1 = require('../images/david.png');
+const cricket2 = require('../images/dhoni.png');
+const cricket3 = require('../images/quinton.png');
+const cricket4 = require('../images/rohit.png');
+const cricket5 = require('../images/virat.png');
+const football1 = require('../images/ronaldo.png');
+
+const nft = [
+      {
+            id: '1',
+            name: comicon1,
+            type: "comicon",
+      },
+      {
+            id: '2',
+            name: comicon2,
+            type: "comicon",
+      },
+      {
+            id: '3',
+            name: comicon3,
+            type: "comicon",
+      },
+      {
+            id: '4',
+            name: cricket1,
+            type: "cricket",
+      },
+      {
+            id: '5',
+            name: cricket2,
+            type: "cricket",
+      },
+      {
+            id: '6',
+            name: cricket3,
+            type: "cricket",
+      },
+      {
+            id: '6',
+            name: cricket4,
+            type: "cricket",
+      },
+      {
+            id: '7',
+            name: cricket5,
+            type: "cricket",
+      },
+      {
+            id: '8',
+            name: football1,
+            type: "football",
+      },
+];
+
+export default  nft;
---
+++ web3-project/src/assets/data/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCuCCQsz55F7w11vfPCVFyJp5Fzx6FqEiw",
+  authDomain: "cryo-2c02a.firebaseapp.com",
+  projectId: "cryo-2c02a",
+  storageBucket: "cryo-2c02a.appspot.com",
+  messagingSenderId: "379773249524",
+  appId: "1:379773249524:web:1b893840ea6e5e2b60fba1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
